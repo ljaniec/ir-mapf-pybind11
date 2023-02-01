3d9aca9dc46b93784a51c4b5c531cd3943c90380
ljaniec <janiec.lukas@gmail.com>
2023-02-01 11:34:10 +0100

    -fPIC problems - test.py file

---
+++ build/test.py
@@ -0,0 +1,11 @@
+from pymapf import process_mapf_problem_instance
+
+instance_file = "../instances/arena_300agents_1.txt"
+solver_name = "PIBT"
+output_file = "result.txt"
+# void process_mapf_problem_instance(std::string instance_file, std::string solver_name,
+#                                    std::string output_file,
+#                                    bool random_starts_goals = false)
+process_mapf_problem_instance(
+    instance_file, solver_name, output_file, random_starts_goals=False
+)
